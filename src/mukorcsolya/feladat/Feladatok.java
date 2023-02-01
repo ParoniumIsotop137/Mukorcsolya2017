5afe682c0070ae47b845a9eb5f28891c5bab033e
Ferenc Paronai <123371391+ParoniumIsotop137@users.noreply.github.com>
2023-02-01 12:51:06 +0100

    v1.0 3, 4, 5, és 6. feladat

--- src/mukorcsolya/feladat/Feladatok.java
+++ src/mukorcsolya/feladat/Feladatok.java
@@ -19,4 +19,38 @@ public class Feladatok {
 		}
 	}
 
+	public void OsszPontszam(List<Versenyzo> rovidProgram, List<Versenyzo> donto, String nev) {
+
+		double osszPontSzam = 0.0;
+
+		for (Versenyzo versenyzo : rovidProgram) {
+			if (versenyzo.getNev().equalsIgnoreCase(nev)) {
+				osszPontSzam = versenyzo.getTechikaiPontszam() + versenyzo.getKomponensPontszam();
+				for (Versenyzo dontos : donto) {
+					if (dontos.getNev().equalsIgnoreCase(nev)) {
+						osszPontSzam += dontos.getTechikaiPontszam() + dontos.getKomponensPontszam();
+					}
+				}
+			}
+		}
+
+		System.out.println("\n6. feladat:\nA versenyző összpontszáma: " + osszPontSzam);
+
+	}
+
+	public void KeresesNevre(List<Versenyzo> donto, List<Versenyzo> rovidProgram, String nev) {
+
+		int i = 0;
+
+		while (i < rovidProgram.size() && !rovidProgram.get(i).getNev().equalsIgnoreCase(nev)) {
+			i++;
+		}
+
+		if (i < rovidProgram.size()) {
+			OsszPontszam(rovidProgram, donto, nev);
+		} else {
+			System.out.println("Ilyen nevű induló nem volt!");
+		}
+
+	}
 }
--- src/mukorcsolya/feladat/Helsinki2017Main.java
+++ src/mukorcsolya/feladat/Helsinki2017Main.java
@@ -1,8 +1,11 @@
 package mukorcsolya.feladat;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Helsinki2017Main {
+
+	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
@@ -20,6 +23,13 @@ public class Helsinki2017Main {
 		System.out.println("2. feladat:\nA rövidprogramban " + rovidProgram.size() + " induló volt");
 		System.out.println("\n3. feladat:");
 		feladat.KeresesMagyarVersenyzo(donto);
+		String nev;
+		System.out.println("\n5. feladat:\n");
+		System.out.print("Kérem a versenyző nevét: ");
+		nev = sc.nextLine();
+
+		feladat.KeresesNevre(donto, rovidProgram, nev);
+
 	}
 
 }
